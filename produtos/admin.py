9f1d76328b99563b369ba4e31041a5bc00d3c9bb
rafa <arkerafa@gmail.com>
2021-07-21 11:15:50 -0300

    preparação para crud

--- produtos/admin.py
+++ produtos/admin.py
@@ -3,7 +3,7 @@ from .models import Categoria, Produto
 
 @admin.register(Produto)
 class ProdutoAdmin(admin.ModelAdmin):
-    list_display = ("nome", "categoria", "slug", "qtd", "disponivel", "criado", "atualizado")
+    list_display = ("nome", "categoria",  "qtd",  "criado", "atualizado")
 
 
 @admin.register(Categoria)
