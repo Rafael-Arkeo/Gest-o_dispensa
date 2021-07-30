5e9f270018375c40d4d46857796581dee260986f
rafa <arkerafa@gmail.com>
2021-07-30 03:51:25 -0300

    CRUD final de Categorias

--- categorias/urls.py
+++ categorias/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from .views import list_categoria, nova_categoria
+from .views import list_categoria, nova_categoria, nova_categoria_alternativa
+from .views import atualizar_categoria, deletar_categoria
 
 
 urlpatterns = [
 
     path('list/', list_categoria, name='list-categoria'),
     path('nova', nova_categoria, name='nova-categoria'),
+    path('nova2', nova_categoria_alternativa, name='nova-categoria2'),
+    path('atualizar/<int:id>/', atualizar_categoria, name='atualizar-categoria'),
+    path('deletar/<int:id>/', deletar_categoria, name='deletar-categoria'),
 ]
--- categorias/views.py
+++ categorias/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from produtos.models import Categoria
 from .forms import CategoriaForm
 
@@ -14,5 +14,39 @@ def nova_categoria(request):
 
     if form.is_valid():
         form.save()
+        return redirect('list-categoria')
+    return render(request, 'nova-categoria.html', {'form': form})
+
+
+def nova_categoria_alternativa(request):
+    """Caso o usuário crie uma nova categoria
+        no formulário do "novo produto",ele será
+            redirecionado para pagina de "nova categoria"
+                então voltará para a pagina anterior"""
+    form = CategoriaForm(request.POST or None)
+
+    if form.is_valid():
+        form.save()
         return redirect('novo_prod')
     return render(request, 'nova-categoria.html', {'form': form})
+
+
+def atualizar_categoria(request, id):
+    cat = get_object_or_404(Categoria, pk=id)
+    form = CategoriaForm(request.POST or None, instance=cat)
+
+    if form.is_valid():
+        form.save()
+        return redirect('list-categoria')
+    return render(request, 'atualizar-categoria.html', {'form': form, 'cat': cat})
+
+
+def deletar_categoria(request, id):
+    cat = get_object_or_404(Categoria, pk=id)
+    form = CategoriaForm(request.POST or None, instance=cat)
+
+    if request.method == 'POST':
+        cat.delete()
+        return redirect('list-categoria')
+
+    return render(request, 'deletar-categoria.html', {'cat': cat})
